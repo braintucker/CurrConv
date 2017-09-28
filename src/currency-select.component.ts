7bcbcc871741fabd2b835bd7ebdb1dffc167b250
Brain Tucker <brian.briantucker@gmail.com>
2017-09-28 13:09:46 -0500

    using ngFor to add currencies to dropdown list, using ngModel to show which selected currecncy is being used

--- src/currency-select.component.ts
+++ src/currency-select.component.ts
@@ -3,13 +3,17 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'currency-select',
   template: `
-    <select>
-      <option>{{selected}}</option>
+    <select [ngModel]="selected" >
+      <option *ngFor="let currency of supportedCurrencies" [value] = "currency">
+        {{currency}}
+      </option>
     </select>
   `
 })
 export class CurrencySelectComponent {
 
   @Input() selected: string;
+
+  supportedCurrencies = ['EUR', 'GBP', 'USD'];
 
 }
